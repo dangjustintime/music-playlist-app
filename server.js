874a9c90b0d53ba2defed9115ab6c985ade17158
Justin Dang <dangjustintime@gmail.com>
2018-07-19 14:57:23 -0400

    connected server.js to mongo, and installed mongoose to packages

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+// dependencies
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+/*
+const todosController = require('./controllers/todos.js');
+app.use('/todos', todosController);
+*/
+
+app.listen(3000, () => {
+    console.log('listening...');
+});
+
+mongoose.connect('mongodb://localhost:27017/playlist_app', { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+    console.log('connected to mongoose');
+});
